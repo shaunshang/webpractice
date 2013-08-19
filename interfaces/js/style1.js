642dd5241f8653a4fff676e4b20fcf22fdcf49e3
Shaun Shang <shaun.shang@tbwa.com>
2013-08-19 12:16:11 +0800

    some practices on tabs 70%

--- interfaces/js/style1.js
+++ interfaces/js/style1.js
@@ -2,8 +2,17 @@ $(function(){
 	$("ul li:last-child").click(function(){
 		$(this).before("<li><a href='javascript:;'><span>New Tab</span><span class='close' onclick='removeTab(this)'>x</span></a></li>");
 	});
+	
+	$("#tabs").sortable({
+		items: "li:not(.add)"
+	});
+	$("#tabs").disableSelection();
 });
 
 function removeTab(obj) {
-	obj.parentNode.parentNode.remove();
+	var count = $("ul li:not(.add)").length;
+	
+	if(count != 1) {
+		obj.parentNode.parentNode.remove();
+	}
 }
