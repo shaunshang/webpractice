8fd899af65b76b66dc14481282b3d4c44a1019dd
Shaun Shang <shaun.shang@tbwa.com>
2013-08-29 12:07:49 +0800

    dashboard 10%

--- interfaces/js/style.js
+++ interfaces/js/style.js
@@ -1,3 +1,35 @@
 $(function(){
+	/*
+	 * start up page
+	 * 
+	 * */
+	$.ajax({
+		async: false,
+		cache: false,
+		type: "GET",
+		url: "dashboard.html",
+		error: function(xml){alert("Error request failed");},
+		timeout: 1000,
+		success: function(e) {
+			$(".body-container").empty().append(e);
+		}
+	});
 	
+	
+	/*
+	 * Start button
+	 * 
+	 * */
+	var open = false;
+	
+	$(".start-btn > a").click(function(){
+		$(".start-menu").fadeToggle("fast");
+		if(open) {
+			$(this).removeClass("active");
+			open = false;
+		} else {
+			$(this).addClass("active");
+			open = true;
+		}
+	});
 });
