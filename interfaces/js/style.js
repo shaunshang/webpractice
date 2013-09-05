a2d6db52c46a1af6e50099495fd9d505d634d365
Shaun Shang <shaun.shang@tbwa.com>
2013-09-05 18:16:24 +0800

    dashboard 15%

--- interfaces/js/style.js
+++ interfaces/js/style.js
@@ -1,5 +1,27 @@
 $(function(){
+	resizeWindow();
+	
+	$(window).resize(function(){
+		resizeWindow();
+	})
+	
 	$("a[data-toggle='dropdown']").click(function(){
 		$(this).next().toggle();
 	});
+	
+	var expand = false;
+	
+	$(".expand a").click(function(){
+		$(".banner-title").toggle();
+	});
 });
+
+function resizeWindow() {
+	var window = $(Window).height();
+	var banner = $(".banner").height();
+	var footer = $(".footer").height();
+	
+	var height = window - banner - footer; 
+	
+	$(".body-container").css("height", height);
+}
