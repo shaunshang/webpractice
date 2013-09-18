e039e5dc25317272ef737593d2210b05acf29656
Shaun Shang <shaun.shang@tbwa.com>
2013-09-18 16:24:25 +0800

    landing page version 1 - 10%

--- interfaces/js/style.js
+++ interfaces/js/style.js
@@ -1,3 +1,10 @@
 $(function(){
+	$(".qtitle").click(function(){
+		$(".question-container > div > div + div").hide();
+		$(this).siblings().animate({
+			
+		});
+	});
 	
+	$(".question-container > div:first-child > *").show();
 });
