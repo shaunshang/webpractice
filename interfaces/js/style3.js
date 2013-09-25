77a12d6057badafc09be8cf6e0b1778a50bed54e
Shaun Shang <shaun.shang@tbwa.com>
2013-09-25 16:16:15 +0800

    landing page version 3 - 50%

--- interfaces/js/style3.js
+++ interfaces/js/style3.js
@@ -15,4 +15,19 @@ $(function(){
 			$(".question").animate({ "left": "-=100%" }, 500, "swing" );
 		}
 	});
+	
+	$("textarea").keyup(function(){
+		var count = $(this).val().replace(/ /g,'').length;
+		
+		if(count > 40) {
+			$(this).addClass("toolong");
+			
+			$(".letter-count").css("color", "red");
+			$(".letter-count span").show();
+		} else {
+			$(this).removeClass("toolong");
+			$(".letter-count").css("color", "rgba(66,68,70,1)");
+			$(".letter-count span").hide();
+		}
+	})
 });
