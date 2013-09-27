969b64fc731099a19872ace7f39b81bc362f51d4
Shaun Shang <shaun.shang@tbwa.com>
2013-09-27 14:21:46 +0800

    landing page version 3 - 80%

--- interfaces/js/style3.js
+++ interfaces/js/style3.js
@@ -1,18 +1,43 @@
 $(function(){
 	var count = 1;
 	var total = $(".question").length;
+	var width = $(window).width()+6;
+	var width1 = $(window).width()+4;
+	var bgcount = 1;
+	
+	$(".background > div").each(function(){
+		var startover;
+		
+		if(bgcount > 4) {
+			bgcount = 1;
+			$(this).css("background", "url('images/create"+bgcount+".png') repeat-x");
+			$(this).css("background-size", "100% 100%");
+		} else {
+			$(this).css("background", "url('images/create"+bgcount+".png') repeat-x");
+			$(this).css("background-size", "100% 100%");
+		}
+		
+		bgcount++;
+	});
+	
 	
 	$("#prev").click(function(){
 		if(count > 1) {
 			count--;
-			$(".question").animate({ "left": "+=100%" }, 500, "swing" );
+			$(".question").animate({ "left": "+="+width+"px" }, 500, "swing" );
+			setTimeout(function(){
+				$(".background > div").animate({ "left": "+="+width1+"px" }, 1200, "swing" );
+			}, 200);
 		}
 	});
 	
 	$("#next").click(function(){
 		if(count < total) {
 			count++;
-			$(".question").animate({ "left": "-=100%" }, 500, "swing" );
+			$(".question").animate({ "left": "-="+width+"px" }, 700, "swing" );
+			setTimeout(function(){
+				$(".background > div").animate({ "left": "-="+width1+"px" }, 1200, "swing" );
+			}, 200);
 		}
 	});
 	
